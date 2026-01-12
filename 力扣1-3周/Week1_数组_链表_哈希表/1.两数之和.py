1482a128ebbf35d2e8ef1fa58358d2e923675dd0
jy-54-st <1990313845@qq.com>
2026-01-12 20:29:22 +0800

    第一天

---
+++ 力扣1-3周/Week1_数组_链表_哈希表/1.两数之和.py
@@ -0,0 +1,35 @@
+"""
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+
+你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+
+你可以按任意顺序返回答案。
+
+
+
+示例 1：
+
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+示例 2：
+
+输入：nums = [3,2,4], target = 6
+输出：[1,2]
+示例 3：
+
+输入：nums = [3,3], target = 6
+输出：[0,1]
+"""
+
+def two_sum(nums,target):
+    hash_map = {}
+    for idx, num in enumerate(nums):
+        diff = target - num
+        if diff in hash_map:
+            return [hash_map[diff], idx]
+        else:
+            hash_map[num] = idx
+    return []
+
+print(two_sum([2,7,11,15], 9))
